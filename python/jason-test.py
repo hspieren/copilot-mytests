a6443b42ba9df22472e6e1bf21ad9c1892805d97
Harry Spierenburg <hjwspierenburg@gmail.com>
2024-08-01 13:59:41 +0200

    copilot: can you document code

--- python/jason-test.py
+++ python/jason-test.py
@@ -9,32 +9,53 @@ class Person:
         self.id = id
         self.email = email
         self.phone_number = phone_number
-    # Function to return a string representation of the person object
-    def __str__(self):
-        return f"Person(name={self.name}, dob={self.dob}, id={self.id}, email={self.email}, phone_number={self.phone_number})"
-    
-    # Function to return a JSON string representation of the person object
-    def to_json(self):
+
+    def to_json(self) -> str:
+        """Convert the Person object to a JSON string."""
         return json.dumps(self.__dict__)
-    
-    # Function to create a person object from a JSON string
+
     @staticmethod
     def from_json(json_str: str):
+        """
+        Create a Person object from a JSON string.
+
+        Args:
+            json_str (str): A JSON string representing a Person.
+
+        Returns:
+            Person: A Person object created from the JSON string.
+        """
         data = json.loads(json_str)
         return Person(data["name"], data["dob"], data["id"], data["email"], data["phone_number"])
 
-# Function write_person_to_file to write a person's information to a file as a JSON string
 def write_person_to_file(person: Person, file_name: str):
+    """
+    Write a Person's information to a file as a JSON string.
+
+    Args:
+        person (Person): The Person object to write to the file.
+        file_name (str): The name of the file to write to.
+    """
     with open(file_name, "w") as file:
         file.write(person.to_json())
 
-# Function read_person_from_file to read a person's information from a file as a JSON string
-def read_person_from_file(file_name: str):
+def read_person_from_file(file_name: str) -> str:
+    """
+    Read a Person's information from a file as a JSON string.
+
+    Args:
+        file_name (str): The name of the file to read from.
+
+    Returns:
+        str: The JSON string read from the file.
+    """
     with open(file_name, "r") as file:
         return file.read()
-    
-# Main function to test the Person class and the write_person_to_file and read_person_from_file functions
+
 def main():
+    """
+    Main function to test the Person class and the write_person_to_file and read_person_from_file functions.
+    """
     # Create a new person object
     person = Person("Jason", "01-01-2000", 12345, "jason@example.com", "123-456-7890")
     print(person)
